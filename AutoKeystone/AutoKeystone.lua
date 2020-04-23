dc5c8fec6cc3b85b37ef2c809546175b04ba66a2
Hydra <ryanandrews135@gmail.com>
2020-04-23 16:55:04 -0600

    Just run this loop directly

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -2,31 +2,19 @@ local GetContainerNumSlots = GetContainerNumSlots
 local GetContainerItemID = GetContainerItemID
 local UseContainerItem = UseContainerItem
 
-local GetKeystoneBagSlot = function()
-	local KeystoneBag
-	local KeystoneSlot
+local OnShow = function(self)
+	local ID
 	
 	for Bag = 0, 4 do
 		for Slot = 1, GetContainerNumSlots(Bag) do
-			local ID = GetContainerItemID(Bag, Slot)
+			ID = GetContainerItemID(Bag, Slot)
 			
 			if (ID and ID == 158923) then
-				KeystoneBag = Bag
-				KeystoneSlot = Slot
+				UseContainerItem(Bag, Slot)
 				
 				break
 			end
 		end
-	end
-	
-	return KeystoneBag, KeystoneSlot
-end
-
-local OnShow = function(self)
-	local Bag, Slot = GetKeystoneBagSlot()
-	
-	if (Bag and Slot) then
-		UseContainerItem(Bag, Slot)
 	end
 end
 
