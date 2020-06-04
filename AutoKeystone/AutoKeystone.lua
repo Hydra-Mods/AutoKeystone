14d3f268b10015a19c1a89e53b9e5520aa7884f4
Hydra <ryanandrews135@gmail.com>
2020-06-04 14:30:01 -0600

    Use global for bag size

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -4,7 +4,7 @@ AutoKeystone:SetScript("OnEvent", function(self, event, addon)
 	if (addon == "Blizzard_ChallengesUI") then
 		if ChallengesKeystoneFrame then
 			ChallengesKeystoneFrame:HookScript("OnShow", function()
-				for Bag = 0, 4 do
+				for Bag = 0, NUM_BAG_SLOTS do
 					for Slot = 1, GetContainerNumSlots(Bag) do
 						local ID = GetContainerItemID(Bag, Slot)
 						
