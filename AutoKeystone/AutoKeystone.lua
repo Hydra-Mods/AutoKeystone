b1f7b37655717b4398cdc61f407b8a27b860a98e
Hydra <ryanandrews135@gmail.com>
2020-08-20 12:19:38 -0600

    move anonymous function to AutoKeystone method

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,19 +1,21 @@
 local AutoKeystone = CreateFrame("Frame")
 
+function AutoKeystone:OnShow()
+	for Bag = 0, NUM_BAG_SLOTS do
+		for Slot = 1, GetContainerNumSlots(Bag) do
+			local ID = GetContainerItemID(Bag, Slot)
+			
+			if (ID and (ID == 158923 or ID == 151086)) then
+				return UseContainerItem(Bag, Slot)
+			end
+		end
+	end
+end
+
 function AutoKeystone:OnEvent(event, addon)
 	if (addon == "Blizzard_ChallengesUI") then
 		if ChallengesKeystoneFrame then
-			ChallengesKeystoneFrame:HookScript("OnShow", function()
-				for Bag = 0, NUM_BAG_SLOTS do
-					for Slot = 1, GetContainerNumSlots(Bag) do
-						local ID = GetContainerItemID(Bag, Slot)
-						
-						if (ID and (ID == 158923 or ID == 151086)) then
-							return UseContainerItem(Bag, Slot)
-						end
-					end
-				end
-			end)
+			ChallengesKeystoneFrame:HookScript("OnShow", self.OnShow)
 			
 			self:UnregisterEvent(event)
 		end
