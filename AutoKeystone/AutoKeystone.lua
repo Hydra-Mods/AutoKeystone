94d5d37a9f816b1c9a7db25ce5618c6c74d02a44
Hydra <ryanandrews135@gmail.com>
2020-10-12 11:26:13 -0600

    1.22

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -7,14 +7,12 @@ AutoKeystone:SetScript("OnEvent", function(self, event, addon)
 				for Bag = 0, NUM_BAG_SLOTS do
 					for Slot = 1, GetContainerNumSlots(Bag) do
 						local ID = GetContainerItemID(Bag, Slot)
-						
 						if (ID and (ID == 158923 or ID == 151086)) then
 							return UseContainerItem(Bag, Slot)
 						end
 					end
 				end
 			end)
-			
 			self:UnregisterEvent(event)
 		end
 	end
