1e37b6432b29f90960f7e279bde539cb2d916fab
Hydra <ryanandrews135@gmail.com>
2020-10-31 12:39:54 -0600

    Reorganize - 1.24

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,19 +1,26 @@
 local AutoKeystone = CreateFrame("Frame")
-AutoKeystone:RegisterEvent("ADDON_LOADED")
-AutoKeystone:SetScript("OnEvent", function(self, event, addon)
+
+function AutoKeystone:OnEvent(event, addon)
 	if (addon == "Blizzard_ChallengesUI") then
 		if ChallengesKeystoneFrame then
-			ChallengesKeystoneFrame:HookScript("OnShow", function()
-				for Bag = 0, NUM_BAG_SLOTS do
-					for Slot = 1, GetContainerNumSlots(Bag) do
-						local ID = GetContainerItemID(Bag, Slot)
-						if (ID and ID == 158923) then
-							return UseContainerItem(Bag, Slot)
-						end
-					end
-				end
-			end)
+			ChallengesKeystoneFrame:HookScript("OnShow", self.OnShow)
+			
 			self:UnregisterEvent(event)
 		end
 	end
-end)
+end
+
+function AutoKeystone:OnShow()
+	for Bag = 0, NUM_BAG_SLOTS do
+		for Slot = 1, GetContainerNumSlots(Bag) do
+			local ID = GetContainerItemID(Bag, Slot)
+			
+			if (ID and ID == 158923) then
+				return UseContainerItem(Bag, Slot)
+			end
+		end
+	end
+end
+
+AutoKeystone:RegisterEvent("ADDON_LOADED")
+AutoKeystone:SetScript("OnEvent", AutoKeystone.OnEvent)
