883d408eefd32eede3ddc3b542ad7818fd306f48
Hydra <ryanandrews135@gmail.com>
2020-12-04 10:46:52 -0700

    Remove BFA keystone ID

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -15,7 +15,7 @@ function AutoKeystone:OnShow()
 		for Slot = 1, GetContainerNumSlots(Bag) do
 			local ID = GetContainerItemID(Bag, Slot)
 			
-			if (ID and (ID == 158923 or ID == 151086)) then
+			if (ID and ID == 151086) then
 				return UseContainerItem(Bag, Slot)
 			end
 		end
