b0b3330e07fe52ce96dccd2d295b32e77feb0474
Hydra <ryanandrews135@gmail.com>
2020-12-23 14:43:08 -0700

    Condensed script

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,16 +1,4 @@
-local AutoKeystone = CreateFrame("Frame")
-
-function AutoKeystone:OnEvent(event, addon)
-	if (addon == "Blizzard_ChallengesUI") then
-		if ChallengesKeystoneFrame then
-			ChallengesKeystoneFrame:HookScript("OnShow", self.OnShow)
-			
-			self:UnregisterEvent(event)
-		end
-	end
-end
-
-function AutoKeystone:OnShow()
+local OnShow = function()
 	for Bag = 0, NUM_BAG_SLOTS do
 		for Slot = 1, GetContainerNumSlots(Bag) do
 			local ID = GetContainerItemID(Bag, Slot)
@@ -22,5 +10,16 @@ function AutoKeystone:OnShow()
 	end
 end
 
-AutoKeystone:RegisterEvent("ADDON_LOADED")
-AutoKeystone:SetScript("OnEvent", AutoKeystone.OnEvent)
+local OnEvent = function(self, event, addon)
+	if (addon == "Blizzard_ChallengesUI") then
+		if ChallengesKeystoneFrame then
+			ChallengesKeystoneFrame:HookScript("OnShow", OnShow)
+			
+			self:UnregisterEvent(event)
+		end
+	end
+end
+
+local AK = CreateFrame("Frame")
+AK:RegisterEvent("ADDON_LOADED")
+AK:SetScript("OnEvent", OnEvent)
