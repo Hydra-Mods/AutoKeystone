7dfa42c404c457a2f0e6b2667076750995a9b1e1
Hydra <ryanandrews135@gmail.com>
2021-05-17 20:41:45 -0600

    1.33

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -7,12 +7,22 @@ AK:SetScript("OnEvent", function(self, event, addon)
 				for Bag = 0, NUM_BAG_SLOTS do
 					for Slot = 1, GetContainerNumSlots(Bag) do
 						local ID = GetContainerItemID(Bag, Slot)
+
 						if (ID and ID == 180653) then
 							return UseContainerItem(Bag, Slot)
 						end
 					end
 				end
 			end)
+
+			if (not ChallengesKeystoneFrame:IsMovable()) then
+				ChallengesKeystoneFrame:SetMovable(true)
+				ChallengesKeystoneFrame:RegisterForDrag("LeftButton")
+				ChallengesKeystoneFrame:SetClampedToScreen(true)
+				ChallengesKeystoneFrame:SetScript("OnDragStart", ChallengesKeystoneFrame.StartMoving)
+				ChallengesKeystoneFrame:SetScript("OnDragStop", ChallengesKeystoneFrame.StopMovingOrSizing)
+			end
+
 			self:UnregisterEvent(event)
 		end
 	end
