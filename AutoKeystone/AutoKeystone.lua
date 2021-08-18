40d62730a94691e1222bc80443038e2cba93d52e
Hydra <ryanandrews135@gmail.com>
2021-08-18 13:19:14 -0600

    Update AutoKeystone.lua

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,29 +1,29 @@
 local AK = CreateFrame("Frame")
 AK:RegisterEvent("ADDON_LOADED")
 AK:SetScript("OnEvent", function(self, event, addon)
-	if (addon == "Blizzard_ChallengesUI") then
+	if addon == "Blizzard_ChallengesUI" then
 		if ChallengesKeystoneFrame then
-			local Frame = ChallengesKeystoneFrame
+			local F = ChallengesKeystoneFrame
 			local ID
 
-			Frame:HookScript("OnShow", function()
+			F:HookScript("OnShow", function()
 				for Bag = 0, NUM_BAG_SLOTS do
 					for Slot = 1, GetContainerNumSlots(Bag) do
 						ID = GetContainerItemID(Bag, Slot)
 
-						if (ID and ID == 180653) then
+						if ID and ID == 180653 then
 							return UseContainerItem(Bag, Slot)
 						end
 					end
 				end
 			end)
 
-			if (not Frame:IsMovable()) then
-				Frame:SetMovable(true)
-				Frame:RegisterForDrag("LeftButton")
-				Frame:SetClampedToScreen(true)
-				Frame:SetScript("OnDragStart", Frame.StartMoving)
-				Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
+			if not F:IsMovable() then
+				F:SetMovable(true)
+				F:RegisterForDrag("LeftButton")
+				F:SetClampedToScreen(true)
+				F:SetScript("OnDragStart", F.StartMoving)
+				F:SetScript("OnDragStop", F.StopMovingOrSizing)
 			end
 
 			self:UnregisterEvent(event)
