90763e7309616a4e674e8c260b0418e2db86c65e
Hydra <ryanandrews135@gmail.com>
2021-10-20 11:37:36 -0600

    1.37

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,22 +1,23 @@
-local AK = CreateFrame("Frame")
-AK:RegisterEvent("ADDON_LOADED")
-AK:SetScript("OnEvent", function(self, event, addon)
+local OnShow = function()
+	local ID
+	
+	for Bag = 0, NUM_BAG_SLOTS do
+		for Slot = 1, GetContainerNumSlots(Bag) do
+			ID = GetContainerItemID(Bag, Slot)
+
+			if ID and ID == 180653 then
+				return UseContainerItem(Bag, Slot)
+			end
+		end
+	end
+end
+
+local OnEvent = function(self, event, addon)
 	if addon == "Blizzard_ChallengesUI" then
 		if ChallengesKeystoneFrame then
 			local F = ChallengesKeystoneFrame
-			local ID
 
-			F:HookScript("OnShow", function()
-				for Bag = 0, NUM_BAG_SLOTS do
-					for Slot = 1, GetContainerNumSlots(Bag) do
-						ID = GetContainerItemID(Bag, Slot)
-
-						if ID and ID == 180653 then
-							return UseContainerItem(Bag, Slot)
-						end
-					end
-				end
-			end)
+			F:HookScript("OnShow", OnShow)
 
 			if not F:IsMovable() then
 				F:SetMovable(true)
@@ -29,4 +30,8 @@ AK:SetScript("OnEvent", function(self, event, addon)
 			self:UnregisterEvent(event)
 		end
 	end
-end)
+end
+
+local AK = CreateFrame("Frame")
+AK:RegisterEvent("ADDON_LOADED")
+AK:SetScript("OnEvent", OnEvent)
