4ce4fa1cf5b497d233cd54fcdf69b0ada20a9663
Hydra <ryanandrews135@gmail.com>
2021-11-24 11:10:28 -0700

    1.39

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,11 +1,11 @@
 local OnShow = function()
 	local ID
-	
+
 	for Bag = 0, NUM_BAG_SLOTS do
 		for Slot = 1, GetContainerNumSlots(Bag) do
 			ID = GetContainerItemID(Bag, Slot)
 
-			if ID and ID == 180653 then
+			if (ID and ID == 180653) then
 				return UseContainerItem(Bag, Slot)
 			end
 		end
@@ -13,22 +13,24 @@ local OnShow = function()
 end
 
 local OnEvent = function(self, event, addon)
-	if addon == "Blizzard_ChallengesUI" then
-		if ChallengesKeystoneFrame then
-			local F = ChallengesKeystoneFrame
+	if (addon ~= "Blizzard_ChallengesUI") then
+		return
+	end
 
-			F:HookScript("OnShow", OnShow)
+	if ChallengesKeystoneFrame then
+		local Frame = ChallengesKeystoneFrame
 
-			if not F:IsMovable() then
-				F:SetMovable(true)
-				F:RegisterForDrag("LeftButton")
-				F:SetClampedToScreen(true)
-				F:SetScript("OnDragStart", F.StartMoving)
-				F:SetScript("OnDragStop", F.StopMovingOrSizing)
-			end
+		Frame:HookScript("OnShow", OnShow)
 
-			self:UnregisterEvent(event)
+		if (not Frame:IsMovable()) then
+			Frame:SetMovable(true)
+			Frame:RegisterForDrag("LeftButton")
+			Frame:SetClampedToScreen(true)
+			Frame:SetScript("OnDragStart", Frame.StartMoving)
+			Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
 		end
+
+		self:UnregisterEvent(event)
 	end
 end
 
