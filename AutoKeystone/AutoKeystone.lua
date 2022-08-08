dc37a935fa964593b3caba2b1398f2f66ba8e3ec
Hydra <ryanandrews135@gmail.com>
2022-08-07 22:31:09 -0600

    1.48

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -1,27 +1,27 @@
-local OnShow = function()
-	local ID, Type, SubType
-
-	for bag = 0, NUM_BAG_SLOTS do
-		for slot = 1, GetContainerNumSlots(bag) do
-			ID = GetContainerItemID(bag, slot)
-
-			if ID then
-				Type, SubType = select(6, GetItemInfo(ID))
-
-				if Type == "Reagent" and SubType == "Keystone" then
-					return UseContainerItem(bag, slot)
-				end
-			end
-		end
-	end
-end
-
-local OnEvent = function(self, event, addon)
+local AK = CreateFrame("Frame")
+AK:RegisterEvent("ADDON_LOADED")
+AK:SetScript("OnEvent", function(self, event, addon)
 	if addon ~= "Blizzard_ChallengesUI" then return end
 
 	if ChallengesKeystoneFrame then
 		local Frame = ChallengesKeystoneFrame
-		Frame:HookScript("OnShow", OnShow)
+		Frame:HookScript("OnShow", function()
+			local ID, Type, SubType
+
+			for bag = 0, NUM_BAG_SLOTS do
+				for slot = 1, GetContainerNumSlots(bag) do
+					ID = GetContainerItemID(bag, slot)
+
+					if ID then
+						Type, SubType = select(6, GetItemInfo(ID))
+
+						if Type == "Reagent" and SubType == "Keystone" then
+							return UseContainerItem(bag, slot)
+						end
+					end
+				end
+			end
+		end)
 
 		if not Frame:IsMovable() then
 			Frame:SetMovable(true)
@@ -33,8 +33,4 @@ local OnEvent = function(self, event, addon)
 
 		self:UnregisterEvent(event)
 	end
-end
-
-local AK = CreateFrame("Frame")
-AK:RegisterEvent("ADDON_LOADED")
-AK:SetScript("OnEvent", OnEvent)
+end)
