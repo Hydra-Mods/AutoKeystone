a3c5ebfcb1211b349613dc2a59ed1c8dc086ad21
Hydra <ryanandrews135@gmail.com>
2022-10-19 10:40:59 -0600

    1.55
    script organization

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -15,40 +15,46 @@ else
 	GetNumSlots = GetContainerNumSlots
 end
 
-local AK = CreateFrame("Frame")
-AK:RegisterEvent("ADDON_LOADED")
-AK:SetScript("OnEvent", function(self, event, addon)
-	if addon ~= "Blizzard_ChallengesUI" then
-		return
-	end
-	
-	if ChallengesKeystoneFrame then
-		local Frame = ChallengesKeystoneFrame
-		Frame:HookScript("OnShow", function()
-			local ID, ClassID, SubClassID
-			
-			for bag = 0, NUM_SLOTS do
-				for slot = 1, GetNumSlots(bag) do
-					ID = GetItemID(bag, slot)
-					
-					if ID then
-						ClassID, SubClassID = select(12, GetItemInfo(ID))
-						if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
-							return UseItem(bag, slot)
-						end
-					end
+local OnShow = function()
+	local ID, ClassID, SubClassID
+
+	for bag = 0, NUM_SLOTS do
+		for slot = 1, GetNumSlots(bag) do
+			ID = GetItemID(bag, slot)
+
+			if ID then
+				ClassID, SubClassID = select(12, GetItemInfo(ID))
+
+				if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
+					return UseItem(bag, slot)
 				end
 			end
-		end)
-		
-		if not Frame:IsMovable() then
+		end
+	end
+end
+
+local OnEvent = function(self, event, addon)
+	if (addon ~= "Blizzard_ChallengesUI") then
+		return
+	end
+
+	if ChallengesKeystoneFrame then
+		local Frame = ChallengesKeystoneFrame
+
+		Frame:HookScript("OnShow", OnShow)
+
+		if (not Frame:IsMovable()) then
 			Frame:SetMovable(true)
 			Frame:SetClampedToScreen(true)
 			Frame:RegisterForDrag("LeftButton")
 			Frame:SetScript("OnDragStart", Frame.StartMoving)
 			Frame:SetScript("OnDragStop", Frame.StopMovingOrSizing)
 		end
-		
+
 		self:UnregisterEvent(event)
 	end
-end)
+end
+
+local AK = CreateFrame("Frame")
+AK:RegisterEvent("ADDON_LOADED")
+AK:SetScript("OnEvent", OnEvent)
