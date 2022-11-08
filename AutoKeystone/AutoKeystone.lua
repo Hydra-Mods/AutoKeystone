797afa5de5862db6a3e0653a7993de5b8b6cae94
Hydra <ryanandrews135@gmail.com>
2022-11-08 10:58:59 -0700

    1.58

--- AutoKeystone/AutoKeystone.lua
+++ AutoKeystone/AutoKeystone.lua
@@ -2,38 +2,36 @@ local GetContainerNumSlots = GetContainerNumSlots
 local GetContainerItemID = GetContainerItemID
 local UseContainerItem = UseContainerItem
 
--- Temporary during DF prepatch
 if C_Container and C_Container.GetContainerNumSlots then GetContainerNumSlots = C_Container.GetContainerNumSlots end
 if C_Container and C_Container.GetContainerItemID then GetContainerItemID = C_Container.GetContainerItemID end
 if C_Container and C_Container.UseContainerItem then UseContainerItem = C_Container.UseContainerItem end
 
-local OnShow = function()
-	local ID, ClassID, SubClassID
-
-	for bag = 0, NUM_BAG_FRAMES do
-		for slot = 1, GetContainerNumSlots(bag) do
-			ID = GetContainerItemID(bag, slot)
-
-			if ID then
-				ClassID, SubClassID = select(12, GetItemInfo(ID))
-
-				if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
-					return UseContainerItem(bag, slot)
-				end
-			end
-		end
-	end
-end
-
-local OnEvent = function(self, event, addon)
+local AK = CreateFrame("Frame")
+AK:RegisterEvent("ADDON_LOADED")
+AK:SetScript("OnEvent", function(self, event, addon)
 	if (addon ~= "Blizzard_ChallengesUI") then
 		return
 	end
 
 	if ChallengesKeystoneFrame then
 		local Frame = ChallengesKeystoneFrame
+		Frame:HookScript("OnShow", function()
+			local ID, ClassID, SubClassID
 
-		Frame:HookScript("OnShow", OnShow)
+			for bag = 0, NUM_BAG_FRAMES do
+				for slot = 1, GetContainerNumSlots(bag) do
+					ID = GetContainerItemID(bag, slot)
+
+					if ID then
+						ClassID, SubClassID = select(12, GetItemInfo(ID))
+
+						if (ClassID == 5 and SubClassID == 1) or (ID == 180653) then
+							return UseContainerItem(bag, slot)
+						end
+					end
+				end
+			end
+		end)
 
 		if (not Frame:IsMovable()) then
 			Frame:SetMovable(true)
@@ -45,8 +43,4 @@ local OnEvent = function(self, event, addon)
 
 		self:UnregisterEvent(event)
 	end
-end
-
-local AK = CreateFrame("Frame")
-AK:RegisterEvent("ADDON_LOADED")
-AK:SetScript("OnEvent", OnEvent)
+end)
